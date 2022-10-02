6dcc535a4661b995580ddfe97fc9d1d9457bab18
Emine Shaban <emineshaban@icloud.com>
2022-10-02 23:01:12 +0300

    fixed errors

--- middlewares/errorHandlerMiddleware.js
+++ middlewares/errorHandlerMiddleware.js
@@ -1,7 +1,9 @@
+const getErrorMessage = (err) => {
+    return err.getErrorMessage
+}
 
-
-exports.errorHandler = (err, req, res, next) =>{
-const status = err.status || 404
-res.status(status).render('404', {error: getErrorMessage(err)})
+exports.errorHandler = (err, req, res, next) => {
+    const status = err.status || 404
+    res.status(status).render('404', { error: getErrorMessage(err) })
 
 }
