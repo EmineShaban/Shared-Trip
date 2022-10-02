122b169dd8f89908f7d5cec78acbd7dd8ccf4249
Emine Shaban <emineshaban@icloud.com>
2022-10-02 10:52:56 +0300

    fix db

--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
 exports.PORT = 3000
-exports.DB_QUERYSTRING = 'mongodb://localhost:27017/artGallery'
+exports.DB_QUERYSTRING = 'mongodb://localhost:27017/sharedTrip'
 exports.SALT_ROUNDS = 10
 exports.SECRET = 'somesecret'
